b0a1bfce54906c163c942cc2871f526f93697d19
ChitownCoder <chitowncoder@gmail.com>
2022-02-01 14:53:12 -0600

    I have learned how to create and run a Java program using Android Studio, next up is Java Code Structure

---
+++ JavaPlayground/LearnJava/src/main/java/dev/chitowncoder/learnjava/MyClass.java
@@ -0,0 +1,9 @@
+package dev.chitowncoder.learnjava;
+
+public class MyClass {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+        System.out.println("Another Day");
+    }
+}
